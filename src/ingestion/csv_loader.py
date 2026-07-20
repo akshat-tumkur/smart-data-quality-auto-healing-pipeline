362f63a9b7725e398da6dfad3221184e181158db
akhilathirichanur <thirichanurakhila@gmail.com>
2026-07-20 20:50:06 +0530

    configs->config

--- src/ingestion/csv_loader.py
+++ src/ingestion/csv_loader.py
@@ -1,1 +1,8 @@
 """CSV ingestion helpers."""
+import pandas as pd
+class CSVLoader:
+    def load(self,file_path):
+        print(f"Reading CSV file from: {file_path}")
+        data=pd.read_csv(file_path)
+        print("CSV loaded succesfully")
+        return data
--- src/ingestion/ingestion_manager.py
+++ src/ingestion/ingestion_manager.py
@@ -1,1 +1,13 @@
-"""Ingestion orchestration entry point."""
+from ingestion.csv_loader import CSVLoader
+class IngestionManager:
+    def __init__(self,config):
+        self.config=config
+    def ingest(self):
+        source= self.config["pipeline"]["source"]
+        if source=="csv":
+            loader=CSVLoader()
+            file_path=self.config["pipeline"]["csv"]["file_path"]
+            return loader.load(file_path)
+        else:
+            raise Exception("Unsupported Source")
+        
